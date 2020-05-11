0bcdc8b22d02a411dfd16b6c7d3827f7fcde210b
NathanKalt <nathan.rhive@gmail.com>
2020-05-11 14:16:38 +0300

    requirements.txt

--- factory/engine.py
+++ factory/engine.py
@@ -1,7 +1,5 @@
 from factory.utils.utils import object_from_settings
 import factory.settings as settings
-from factory.workers.workers import SampleWorker
-from aiomultiprocess import Pool
 from multiprocessing import Queue
 import asyncio
 from asyncio import Queue
@@ -24,7 +22,6 @@ class Engine:
     async def run(self):
         asyncio.ensure_future(self.feed.start(self.settings.FEED_TOPIC, self.feed_queue))
         asyncio.ensure_future(self.stream.start(self.settings.STREAM_TOPIC, self.stream_queue))
-            # self.worker_instance.start()
         async with asyncpool.AsyncPool(loop, num_workers=self.settings.AMOUNT_OF_WORKERS,
                                        name="proc",
                                        logger=logging.getLogger("proc"),
@@ -37,9 +34,6 @@ class Engine:
                                        ) as pool:
             for i in self.feed_queue:
                 await pool.push(i, self.stream_queue)
-
-                # await pool.push(i, result_queue)
-
 
 
 async def run_engine():
